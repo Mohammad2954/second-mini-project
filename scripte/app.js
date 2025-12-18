e52251770c03384a3f3fb6a56e1e80bb56f8fa24
mohammadjavad <mohamad2134javad@gmail.com>
2025-12-18 22:31:05 +0330

    add portfolio

--- scripte/app.js
+++ scripte/app.js
@@ -2,6 +2,7 @@ const navToggleIcon=document.querySelector(".nav__toggle-icon")
 const menuOpen=document.querySelector(".menu")
 const cover=document.querySelector(".cover")
 const resumeListItems=document.querySelectorAll(".resume-list__item")
+const portfoliolistitem=document.querySelectorAll(".portfolio-list__item")
 navToggleIcon.addEventListener("click",function(){
     navToggleIcon.classList.toggle("nav-open")
     menuOpen.classList.toggle("menu--open")
@@ -14,5 +15,14 @@ resumeListItems.forEach(item=>{
         this.classList.add("resume-list__item--active")
         let contentId=item.getAttribute("data-content")
         document.querySelector(contentId).classList.add("resume-content--show")
+    })
+})
+portfoliolistitem.forEach(item=>{
+    item.addEventListener("click",()=>{
+        document.querySelector(".portfolio-list__item--active").classList.remove("portfolio-list__item--active")
+        document.querySelector(".portfolio-content--show").classList.remove("portfolio-content--show")
+        item.classList.add("portfolio-list__item--active")
+        let id=item.getAttribute("id-conent")
+        document.querySelector(id).classList.add("portfolio-content--show")
     })
 })
