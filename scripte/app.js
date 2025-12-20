c27cf592a4bc6898c330ac5d0dd682afadfc62b1
mohammadjavad <mohamad2134javad@gmail.com>
2025-12-20 19:50:33 +0330

    add dark mode

--- scripte/app.js
+++ scripte/app.js
@@ -5,6 +5,8 @@ const resumeListItems=document.querySelectorAll(".resume-list__item")
 const portfoliolistitem=document.querySelectorAll(".portfolio-list__item")
 const menuItem=document.querySelectorAll(".menu__item")
 const sections = document.querySelectorAll("main > section")
+const chengThem=document.querySelector(".cheng-them")
+
 navToggleIcon.addEventListener("click",function(){
     navToggleIcon.classList.toggle("nav-open")
     menuOpen.classList.toggle("menu--open")
@@ -43,4 +45,33 @@ function observerHandler(allSections) {
             sectionMenuItem.classList.remove("menu-active")
         }
     })
+}
+menuItem.forEach(item => {
+    item.addEventListener("click", function (e) {
+        e.preventDefault()
+        removeActive('menu-active')
+        item.classList.add("menu-active")
+        let sectionClass = item.getAttribute("data-section")
+        let sectionOffsetTop = document.querySelector(`.${sectionClass}`).offsetTop
+
+        window.scrollTo({
+            top: sectionOffsetTop - 130,
+            behavior: "smooth"
+        })
+    })
+})
+chengThem.addEventListener("click",()=>{
+    document.documentElement.classList.toggle("dark")
+    if(document.documentElement.classList.contains("dark")){
+        window.localStorage.setItem("teme","dark")
+        chengThem.innerHTML=`<svg viewBox="0 0 24 24" stroke-width="1.5" class="theme size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>`
+    }else{
+        window.localStorage.setItem("teme","light")
+        chengThem.innerHTML=`<svg viewBox="0 0 24 24" stroke-width="1.5"  class="size-6 theme"><path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>`
+    }
+})
+if(window.localStorage.getItem("teme")=="dark"){
+    document.documentElement.classList.add("dark")
+    chengThem.innerHTML=`<svg viewBox="0 0 24 24" stroke-width="1.5" class="theme size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>`
+    
 }
