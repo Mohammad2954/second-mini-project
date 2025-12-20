3371023d2bb8ff2a18dfd2485d87ccdbbea25031
mohammadjavad <mohamad2134javad@gmail.com>
2025-12-20 12:51:02 +0330

    add js for header

--- scripte/app.js
+++ scripte/app.js
@@ -3,26 +3,39 @@ const menuOpen=document.querySelector(".menu")
 const cover=document.querySelector(".cover")
 const resumeListItems=document.querySelectorAll(".resume-list__item")
 const portfoliolistitem=document.querySelectorAll(".portfolio-list__item")
+const menuItem=document.querySelectorAll(".menu__item")
 navToggleIcon.addEventListener("click",function(){
     navToggleIcon.classList.toggle("nav-open")
     menuOpen.classList.toggle("menu--open")
     cover.classList.toggle("cover--show")
 })
-resumeListItems.forEach(item=>{
-    item.addEventListener("click",function(){
-        document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active")
-        document.querySelector(".resume-content--show").classList.remove("resume-content--show")
-        this.classList.add("resume-list__item--active")
-        let contentId=item.getAttribute("data-content")
-        document.querySelector(contentId).classList.add("resume-content--show")
+function navigationTabInit(listItems,activeClass,showClass,dataGet){
+    listItems.forEach(listItem=>{
+        listItem.addEventListener("click",()=>{
+            removeActive(activeClass)
+            removeActive(showClass)
+            listItem.classList.add(`${activeClass}`)
+            let id=listItem.getAttribute(`${dataGet}`)
+            document.querySelector(id).classList.add(`${showClass}`)
+        })
     })
-})
-portfoliolistitem.forEach(item=>{
-    item.addEventListener("click",()=>{
-        document.querySelector(".portfolio-list__item--active").classList.remove("portfolio-list__item--active")
-        document.querySelector(".portfolio-content--show").classList.remove("portfolio-content--show")
-        item.classList.add("portfolio-list__item--active")
-        let id=item.getAttribute("id-conent")
-        document.querySelector(id).classList.add("portfolio-content--show")
+}
+function removeActive(removclass){
+    document.querySelector(`.${removclass}`).classList.remove(`${removclass}`)
+
+}
+navigationTabInit(portfoliolistitem,"portfolio-list__item--active","portfolio-content--show","id-conent")
+navigationTabInit(resumeListItems,"resume-list__item--active","resume-content--show","data-content")
+menuItem.forEach(item=>{
+    item.addEventListener("click",(e)=>{
+        e.preventDefault()
+        removeActive("menu-active")
+        item.classList.add("menu-active")
+        let data=item.getAttribute("data-section")
+        let scrollTop=document.querySelector(`.${data}`).offsetTop
+        window.scrollTo({
+            top:scrollTop-120,
+            behavior:"smooth"
+        })
     })
 })
